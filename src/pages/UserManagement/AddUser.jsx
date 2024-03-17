e1f8b8335d3674be07fa4bd92611c20e829b3768
suron <suron.maharjan@gmail.com>
2024-03-17 10:14:53 +0545

    username value

--- src/pages/UserManagement/AddUser.jsx
+++ src/pages/UserManagement/AddUser.jsx
@@ -1,34 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const AddUser = () => {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
 
-    const [username, setUsername] = useState();
-    const [email, setEmail] = useState();
+  const handleUserName = (e) => {
+    setUsername(e.target.value);
+  };
 
+  const saveForm = () => {
+    console.log("saveform");
+    console.log("Username: ", username);
+  };
 
-    const saveForm = () => {
-        console.log("hellos");
+  return (
+    <>
+      <h1>Add user</h1>
+      <div>
+        <label>Username</label>
+        <input type="text" name="username" onChange={handleUserName} />
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="text" name="email" />
+      </div>
 
-    }
-
-
-    return(
-        <>
-            <h1>Add user</h1>
-            <div>
-                <label>Username</label>
-                <input type="text" name="username" value={username}/>
-            </div>
-            <div>
-                <label>Email</label>
-                <input type="text" name="email"/>
-            </div>
-
-            <div>
-                <button onClick={saveForm}>Save</button>
-            </div>
-        </>
-    )
-}
+      <div>
+        <button onClick={saveForm}>Save</button>
+      </div>
+    </>
+  );
+};
 
 export default AddUser;
